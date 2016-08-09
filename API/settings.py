70219db5f2d29c8a23331ea80dcdb483e51c730e
lucas63 <lucas63@yandex.com>
2016-08-09 20:54:41 +0300

    API basic

--- API/settings.py
+++ API/settings.py
@@ -45,7 +45,11 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    )
 }
 
 MIDDLEWARE_CLASSES = [
