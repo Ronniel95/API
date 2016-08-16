a80a4e4bc3ddf1ea70fc17d854fff45554bb7cf5
lucas63 <lucas63@yandex.com>
2016-08-16 13:15:18 +0300

    DB migrations

--- BillSays/views.py
+++ BillSays/views.py
@@ -430,4 +430,13 @@ class UsersAPIListView(APIView):
         return Response(serializer.errors, status=400)
 
 
+from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import response, schemas
+
+@api_view()
+@renderer_classes([SwaggerUIRenderer, OpenAPIRenderer])
+def schema_view(request):
+    generator = schemas.SchemaGenerator(title='Pastebin API')
+    return response.Response(generator.get_schema(request=request))
 
