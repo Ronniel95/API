d42fadb03e1631577c8033a3f7bbcd6bafdab99b
lucas63 <lucas63@yandex.com>
2016-08-16 13:08:23 +0300

    DB migrations

--- BillSays/urls.py
+++ BillSays/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework.routers import SimpleRouter
-
+import rest_framework_swagger
 from BillSays import views
 from views import schema_view
 
