f00b864cd17c5e657af3d238d78ef00c367e6bd1
lucas63 <lucas63@yandex.com>
2016-08-16 13:50:07 +0300

    DB migrations

--- API/settings.py
+++ API/settings.py
@@ -168,19 +168,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 
-#STATIC_URL = '/static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT=os.path.join(BASE_DIR,'Checks/')
 MEDIA_URL = '/Checks/'
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
