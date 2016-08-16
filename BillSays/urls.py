6c9233ee0b8c70a982bacd42965f2653bcc7e26a
lucas63 <lucas63@yandex.com>
2016-08-16 13:19:14 +0300

    DB migrations

--- BillSays/urls.py
+++ BillSays/urls.py
@@ -38,6 +38,6 @@ urlpatterns = patterns('',
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
-    url(r'^api-docs/', include('rest_framework_swagger.urls'))
+    url(r'^api-docs/', include('rest_framework_swagger.urls')),
 
 )
