735a6dc688f8cc63bdbe0a41f724263416d443f6
lucas63 <lucas63@yandex.com>
2016-08-16 12:10:22 +0300

    DB migrations

--- API/settings.py
+++ API/settings.py
@@ -87,6 +87,7 @@ WSGI_APPLICATION = 'API.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
+
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':
