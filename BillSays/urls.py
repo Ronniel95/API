8986a764d1b4eb8063b8e6a297daa763f24d02a5
lucas63 <lucas63@yandex.com>
2016-08-24 02:53:27 +0300

    Added registration + facebook integration

--- BillSays/urls.py
+++ BillSays/urls.py
@@ -6,6 +6,7 @@ from rest_framework.routers import SimpleRouter
 import rest_framework_swagger
 from rest_framework_jwt.views import obtain_jwt_token
 
+
 from API import settings
 from BillSays import views
 from views import schema_view, FacebookLogin
@@ -15,20 +16,16 @@ from rest_framework import renderers, response, schemas
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 from allauth.account.views import confirm_email as allauthemailconfirmation
-
-@api_view()
-@renderer_classes([SwaggerUIRenderer, OpenAPIRenderer, renderers.CoreJSONRenderer])
-def schema_view(request):
-    generator = schemas.SchemaGenerator(title='Pastebin API')
-    return response.Response(generator.get_schema(request=request))
-
-
 from django.views.generic import TemplateView, RedirectView
+
+
 
 urlpatterns = patterns('',
 
-    url(r'^', include(router.urls)),
+    url(r'^$', TemplateView.as_view(template_name="home.html"), name='home'),
+    #url(r'^', include(router.urls)),
 
+    #swagger documentation for API
     url('^docs/', schema_view),
 
     #loginning
@@ -37,20 +34,17 @@ urlpatterns = patterns('',
     #registration
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
 
-
     # login via facebook
     url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
 
     # admin site
     url(r'^admin/', include(admin.site.urls)),
 
-
+    #allauth needed
     url(r'^accounts/', include('allauth.urls')),
 
+    #email verification
+    url(r'^email-verification/$',TemplateView.as_view(template_name="email_verification.html"),name='email-verification'))
 
-    url(r'^email-verification/$',TemplateView.as_view(template_name="email_verification.html"),
-                           name='email-verification'),)
-
-urlpatterns += patterns('',
-    (r'^static/(?P<path>.*)$','django.views.static.serve',{'document_root':settings.STATIC_ROOT,'show_indexes': False}),
-)
+urlpatterns += patterns('',(r'^static/(?P<path>.*)$','django.views.static.serve',
+     {'document_root':settings.STATIC_ROOT,'show_indexes': False})),
