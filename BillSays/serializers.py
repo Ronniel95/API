e7ab81de37397e5de0dc274d58aba2bc861bcd72
lucas63 <lucas63@yandex.com>
2016-08-31 00:16:37 +0300

    Adding Friend functions

--- BillSays/serializers.py
+++ BillSays/serializers.py
@@ -24,8 +24,10 @@ class FriendSerializer(serializers.ModelSerializer):
 
     def is_valid(self, raise_exception=False):
 
-        if(User.objects.filter(id == self.initial_data['fk_user_owner']).count() !=1):
+        if User.objects.filter(id=self.initial_data['fk_user_friend']).count() != 1:
             return False
+
+        if
 
 
         return super(FriendSerializer, self).is_valid(raise_exception)
