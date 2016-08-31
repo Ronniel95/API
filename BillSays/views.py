da04fc194fb5c817864f010e1692db2a30f0ec40
lucas63 <lucas63@yandex.com>
2016-08-31 17:21:40 +0300

    Check uploading

--- BillSays/views.py
+++ BillSays/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
+from rest_framework.viewsets import ModelViewSet
 from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import response, schemas
@@ -120,10 +121,9 @@ class CheckAPIListView(APIView):
         return Response(serializer.errors, status=400)
 
 
-import rest_framework
 
 
-class CheckViewSet(rest_framework.viewsets.ModelViewSet):
+class CheckViewSet(ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
