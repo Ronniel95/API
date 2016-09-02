96b0aaf5e6d338733d88ac8285e272de7729c273
lucas63 <lucas63@yandex.com>
2016-09-02 04:19:07 +0300

    Check uploading

--- BillSays/serializers.py
+++ BillSays/serializers.py
@@ -31,4 +31,4 @@ class RecognizedCheckSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Check
-        fields = ('fk_user', 'date_created', 'image_url','fk_waitress','total_cost', 'dishes')
+        fields = ('dishes')
