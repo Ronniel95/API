c4e680d9c4ca4b51683a91c5d407ca942115c6f0
lucas63 <lucas63@yandex.com>
2016-09-02 03:05:16 +0300

    Check uploading

--- BillSays/views.py
+++ BillSays/views.py
@@ -112,7 +112,7 @@ class CheckViewSet(viewsets.ModelViewSet):
         if serializer.is_valid():
             test = serializer.save()
             self.generate_check(test)
-            return Response(CheckSerializer(test))
+            return Response(CheckSerializer(instance=test))
         else:
             return Response(serializer.errors, status=400)
 
