df1f8145e6729ade149342a2408964e6dec6fd5e
lucas63 <lucas63@yandex.com>
2016-09-04 15:37:38 +0300

    Sending spited data

--- BillSays/models.py
+++ BillSays/models.py
@@ -2,6 +2,9 @@
 from __future__ import unicode_literals
 
 from allauth.account.signals import user_logged_in
+from allauth.account.utils import perform_login
+from allauth.socialaccount.app_settings import app_settings
+from allauth.socialaccount.signals import pre_social_login
 from django.db import models
 
 # Create your models here.
@@ -166,27 +169,9 @@ def update_username_from_email(sender, instance, **kwargs):
 
 
 
-@receiver(user_logged_in, dispatch_uid="allauth.account.signals.user_logged_in")
-def auto_connect_social_account(request, user, **kwargs):
-    """
-    If user sucessfully login via social network, but he already has account (based on email),
-    he is provoke to login via email. After logged in, this signal create connection between social network and user account.
-    Next time user can use social network or email to login.
-    """
-
-    sociallogin = None
-    data = request.session.get('socialaccount_sociallogin', None)
-    if data:
-        from allauth.socialaccount.models import SocialLogin
-        sociallogin = SocialLogin.deserialize(data)
-
-    if not sociallogin:
-        return
-
-    if user.email != sociallogin.user.email:
-        return
-
-    from allauth.socialaccount.helpers import _add_social_account
-    _add_social_account(request, sociallogin)
-
-    del request.session['socialaccount_sociallogin']
+@receiver(pre_social_login)
+def link_to_local_user(sender, request, sociallogin, **kwargs):
+    email_address = sociallogin.account.extra_data['email']
+    users = User.objects.filter(email=email_address)
+    if users:
+        perform_login(request, users[0], email_verification=app_settings.EMAIL_VERIFICATION)
