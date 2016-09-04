9e941ca0e7c8c538bbc8a36a77fb9bf727d01a11
lucas63 <lucas63@yandex.com>
2016-09-04 17:13:24 +0300

    Sending spited data

--- API/settings.py
+++ API/settings.py
@@ -182,7 +182,7 @@ SWAGGER_SETTINGS = {
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
-
+"""
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -197,6 +197,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+"""
 
 
 # Internationalization
