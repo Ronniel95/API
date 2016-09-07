3fdc0daf51dda41238951672f411199504b76c54
lucas63 <lucas63@yandex.com>
2016-09-08 00:42:04 +0300

    Sending spited data

--- API/settings.py
+++ API/settings.py
@@ -86,8 +86,10 @@ REST_FRAMEWORK = {
 
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
-        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        #'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.OAuth2Authentication',
         'rest_framework.authentication.TokenAuthentication',
     ),
 
